6126932819bd7ffb656e671d2d7f88c9a00f636e
Tanjim Alam <122804862+Tanjim099@users.noreply.github.com>
2024-01-11 12:07:52 +0530

    updated

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.use("/api/v1/stat", statsRoutes);
 // })
 
 app.use("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/dist/index.htnl"));
+    res.sendFile(path.join(__dirname, "./client/dist/index.html"));
 })
 
 //port
